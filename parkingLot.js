ce6a0915e36ef58c79c24c6d7c5ac62f82ed0b81
github username <github email address>
2020-10-22 13:54:56 -0700

    working on edit class fetches. everything fucked atm

--- parkingLot.js
+++ parkingLot.js
@@ -24,70 +24,63 @@ class EditClass extends React.Component {
       classRoster: this.props.thisClass,
       allStudents:this.props.studentBody,
       thisPeriod:this.props.thisPeriod,
-      newKids:[],
-      goneKids:''
     })
   }
 
   addToClass = (e)=> {
     let currentClass=this.state.classRoster
     let studentBody=this.state.allStudents
-    // if (this.state.newKids=''){let newKids=["hat"]}else{let newKids = this.state.newKids}
-    // console.log(newKids)
-    // let newKids= this.state.newKids
-    // let addAKid= (newKids.push(e))
-    // console.log(addAKid)
-    let newKids=this.state.newKids
-    newKids.push(e)
-    // console.log(newKids)
     let newBody=studentBody.filter(studentBody => studentBody !== e)
-    currentClass.push(e) 
-    this.setState(
-      {classRoster:currentClass, 
-      allStudents:newBody,
-      newKids:newKids})
-      // newKids:addAKid})
+    currentClass.push(e)
+    this.setState({classRoster:currentClass, allStudents:newBody})
   }
 
   removeFromClass = (e) => {
     let currentClass=this.state.classRoster
     let studentBody=this.state.allStudents
-    let goneKids=this.state.goneKids
     let newClass=currentClass.filter(currentClass => currentClass !== e)
-    studentBody.push(e) 
-    goneKids.push(e)
-    this.setState(
-      {classRoster:newClass, 
-      allStudents:studentBody,
-      goneKids:goneKids})
+    studentBody.push(e)
+    this.setState({classRoster:newClass, allStudents:studentBody})
   }
 
   setClass=(e)=>{
-    this.register()
-    // let currentClass=this.state.newKids
-    // let studentBody=this.state.allStudents
-    // currentClass.map(student=>{this.register(student)})
+    let currentClass=this.state.classRoster
+    let studentBody=this.state.allStudents
+    currentClass.map(student=>{this.register(student)})
+    // this.deRegister()
   }
 
+  // deRegister=(e)=>{
+  //   this.props.thisClass.map(student=>{
+  //     if (this.state.classRoster.includes(student) == false) 
+  //       console.log(student)})
+  // }
+
+  // filterAssessments(list){
+  //   let floorScore = ''
+  //     // map through list of students to establish the lowest # of assessments taken
+  //   list.map(student => {
+  //     if (floorScore == ''){floorScore = student[1]}
+  //       else{
+  //     if (student[1] < floorScore){floorScore = student[1]}}
+  //   })
+  //     // send the list and the established lowest score to the function that creates the final list.
+  //   this.eligibleList(list, floorScore)
+  // }
+
   register = (e) => {
-    // console.log(this.state.newKids)
-    let mapMe=this.state.newKids
-    // mapMe.map(student=>{console.log(student.name)})
     // console.log(e)
     // this.props.sendRegistration({class_period_id:this.state.thisClass.id, student_id:e.id})
         // console.log(e)
     // this.props.sendRegistration({class_period_id:this.state.thisClass.id, student_id:e.id})
-    
-    // keep below!!!!!!
-    // console.log(mapMe)
-    // mapMe.map(student=>{console.log(student.name)})
-    mapMe.map(student=> {
     const registration = {
       class_period_id: this.state.thisPeriod,
-      student_id: student.id,
+      student_id: e.id,
       }
-    this.props.sendRegistration(registration)     
-  })
+    this.props.sendRegistration(registration)  
+    this.setState({
+      classRoster:this.props.thisClass
+    })
   }
 
   render(){
