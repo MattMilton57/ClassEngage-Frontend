e8249f7fb3a450391e5a6b1c83b164606c2a50ac
MattMilton <mattmilton57@gmail.com>
2021-06-17 12:57:23 -0700

    continuing cleanup of code

--- src/Containers/ClassStatsContainer.js
+++ src/Containers/ClassStatsContainer.js
@@ -6,51 +6,43 @@ import Roster from "../components/Roster"
 import TitleBox from "../components/TitleBox"
 import TotalAssessments from '../components/TotalAssessments';
 
-const  ClassStatsContainer = ({
-  assessments, 
-  callback, 
-  classObject, 
-  classPeriod, 
-  graphInfoData, 
-  roster, 
-  stateLables,
-  url, 
-}) => {
+const  ClassStatsContainer = ({assessments, callback, classObject, classPeriod, graphInfoData, roster, stateLables, url}) => {
 
   const checkForStudents = () => {
-    if(roster.length === 0)
-      {return(<div className="class-stats__instructions" >
-        <div className="class-stats__instructions--welcome">
-          Welcome to Class Engage
-        </div>
-        <div className="class-stats__instructions--guide">
-          click on the edit class tab to the left to create and register some students
-        </div>
-      </div>)
-      }
-    else 
-      if(assessments.length === 0)
-        {return(
+    if(roster.length === 0){
+      return(
         <div className="class-stats__instructions" >
+          <div className="class-stats__instructions--welcome">
+            Welcome to Class Engage
+          </div>
+          <div className="class-stats__instructions--guide">
+            click on the edit class tab to the left to create and register some students
+          </div>
+        </div>
+      )
+    }else 
+      if(assessments.length === 0){
+        return(
+          <div className="class-stats__instructions" >
             <div className="class-stats__instructions--welcome">
               You have a roster!
             </div>
             <div className="class-stats__instructions--guide">
             click on the assess class tab to the left to assess some students and enable statistics
             </div>
-          </div>)
-        }
-    else
-      {return( 
+          </div>
+        )
+    }else{
+      return( 
         <div className="class-stats__total-participation-shell ">
           <ClassScore
             roster={roster} 
             assessments={assessments}
             classPeriod={classPeriod}
-            />
+          />
         </div>         
-        )
-      }
+      )
+    }
   }
 
   return(
@@ -68,26 +60,21 @@ const  ClassStatsContainer = ({
           />
         </div>
       </div>
-
       <div className="class-stats__class-name">
         <TitleBox title={classObject.subject}/>
       </div>
-
       <div className="class-stats__total-participation">
         {checkForStudents()}
       </div>
-
       <div className="class-stats__graphics">
         <Graphics 
           graphData={graphInfoData}
           stateLables={stateLables}
         />
       </div>
-
       <div className="class-stats__total-assessments">
         <TotalAssessments assessments={assessments}/>
       </div>
-
       <div className="class-stats__last-assessment">
         <LastAssessment
           assessment={assessments[assessments.length-1]}
