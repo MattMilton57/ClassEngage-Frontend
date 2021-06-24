2ad07f7dc500eed687d0743af62744368a54ea99
MattMilton <mattmilton57@gmail.com>
2021-06-23 18:30:48 -0700

    styling links in footer

--- src/components/ContactFooter.js
+++ src/components/ContactFooter.js
@@ -1,30 +1,25 @@
 import React from 'react';
-
 const ContactFooter = ({}) => {
-
     return(
         <div className="navBar__profile">
             <div className="navBar__profile-links">
                 <div className="navBar__profile-text">
-                    Class Engage was built by Matt Milton. It was initially concieved and created as a final project for the Flatiron School Full Stack Software Engineering Bootcamp, Seattle Campus, 2020.
-                </div>
-                <div className="navBar__profile-resume">
-                    <a href="">
-                        <div className="">
-                            Future Resume Link
+                    Class Engage was built by Matt Milton. It was initially concieved and created as a final project for the Flatiron School Full Stack Software Engineering Bootcamp, Seattle Campus, 2020. 
+                    For employment inqueries, Matt can be reached at 
+                        <div className="navBar__profile-text-email">
+                            MattMilton57@gmail.com
                         </div>
+                </div>
+                <div className="navBar__profile-github">
+                    <a className="navBar__profile-github-link" href="https://github.com/MattMilton57/ClassEngage-Frontend">
+                        <span >
+                            github frontend
+                        </span>
                     </a>
                 </div>
                 <div className="navBar__profile-github">
-                    <a href="https://github.com/MattMilton57/ClassEngage-Frontend">
-                        <div className="">
-                            github frontend
-                        </div>
-                    </a>
-                </div>
-                <div className="navBar__profile-specs">
-                    <a href="https://github.com/MattMilton57/classEngageDeploy-backendAPI">
-                        <div className="">
+                    <a className="navBar__profile-github-link" href="https://github.com/MattMilton57/classEngageDeploy-backendAPI">
+                        <div>
                             github backend API
                         </div>
                     </a>
